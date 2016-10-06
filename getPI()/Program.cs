5d6d828815f0c74648ff7c4de82d36334a1302f5
DocCoding <doccoding@outlook.com>
2016-10-06 15:02:26 +0200

    Added Leibniz Formula

--- getPI()/Program.cs
+++ getPI()/Program.cs
@@ -10,6 +10,48 @@ namespace getPI__
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to calcPI()! Would you like to calculate PI using the Leibniz formula (\"LF\") or use a geometric approach (\"G\")?");
+            bool valid = false;
+            while (!valid)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "LF":
+                        Console.Write("Please enter the number of iterations of the Leibniz formula:");
+                        ulong steps = ulong.Parse(Console.ReadLine());
+                        double pi = getPILeibniz(steps);
+                        Console.WriteLine($"PI ~ {pi}");
+                        valid = true;
+                        break;
+                    case "G":
+
+                        valid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static double getPILeibniz(ulong steps)
+        {
+            double piQuarters = 0;
+            double sign = 1;
+            for (ulong i = 1; i <= steps; i += 2)
+            {
+                piQuarters += sign / i;
+                sign = -sign;
+            }
+            return 4 * piQuarters;
+        }
+
+        static double getPiGeometric(double width)
+        {
+            double piQuarters = 0;
+
+            return 4 * piQuarters;
         }
     }
 }
