664b78dbe8d3b7fff4b1703333330bddb1886f9f
DocCoding <doccoding@outlook.com>
2016-10-06 16:06:34 +0200

    Added geometric function to calculate PI

--- getPI()/Program.cs
+++ getPI()/Program.cs
@@ -12,19 +12,22 @@ namespace getPI__
         {
             Console.WriteLine("Welcome to calcPI()! Would you like to calculate PI using the Leibniz formula (\"LF\") or use a geometric approach (\"G\")?");
             bool valid = false;
+            decimal pi = 0;
+            ulong steps = 0;
             while (!valid)
             {
                 switch (Console.ReadLine())
                 {
                     case "LF":
                         Console.Write("Please enter the number of iterations of the Leibniz formula:");
-                        ulong steps = ulong.Parse(Console.ReadLine());
-                        double pi = getPILeibniz(steps);
-                        Console.WriteLine($"PI ~ {pi}");
+                        steps = ulong.Parse(Console.ReadLine());
+                        pi = getPILeibniz(steps);
                         valid = true;
                         break;
                     case "G":
-
+                        Console.Write("Please enter the number of rectangles you want to fit into one quarter of the circle:");
+                        steps = ulong.Parse(Console.ReadLine());
+                        pi = getPIGeometric(steps);
                         valid = true;
                         break;
                     default:
@@ -32,13 +35,14 @@ namespace getPI__
                         break;
                 }
             }
+            Console.WriteLine($"PI ~ {pi}");
             Console.ReadKey();
         }
 
-        static double getPILeibniz(ulong steps)
+        static decimal getPILeibniz(ulong steps)
         {
-            double piQuarters = 0;
-            double sign = 1;
+            decimal piQuarters = 0;
+            decimal sign = 1;
             for (ulong i = 1; i <= steps; i += 2)
             {
                 piQuarters += sign / i;
@@ -47,10 +51,14 @@ namespace getPI__
             return 4 * piQuarters;
         }
 
-        static double getPiGeometric(double width)
+        static decimal getPIGeometric(ulong steps)
         {
-            double piQuarters = 0;
-
+            decimal piQuarters = 0;
+            decimal width = Decimal.Divide(1,steps);
+            for (ulong i = 1; i <= steps; i++)
+            {
+                piQuarters += (decimal)((double)width * Math.Sqrt(1 - Math.Pow(((double)width * i),2)));
+            }
             return 4 * piQuarters;
         }
     }
